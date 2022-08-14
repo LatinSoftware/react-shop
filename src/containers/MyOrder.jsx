9a1c765a3773ddcb161a671164dc15462b198398
Caonabo Mena <cmena7512@gmail.com>
2022-08-14 13:31:28 -0400

    [UPDATE] quita el menu cuando se cliquea el boton de volver

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -2,14 +2,19 @@ import React, { useContext, useEffect } from 'react';
 import OrderItem from '@components/OrderItem';
 import '@styles/MyOrder.scss';
 import icon from '@icons/flechita.svg'
-import AppContext from '../context/AppContext';
-const MyOrder = () => {
+import AppContext from '@context/AppContext';
+const MyOrder = ({toggleHandler}) => {
 	const { state } = useContext(AppContext);
 	var total = state.cart.map(item => item.price).reduce((prev, curr) => prev + curr, 0);
+	const handleHide = () => {
+		var order = document.getElementsByClassName("MyOrder")[0];
+		order.classList.toggle("hide");
+		toggleHandler(false);
+	}
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
-				<img src={icon} alt="arrow" />
+				<img src={icon} alt="arrow" onClick={handleHide} />
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
